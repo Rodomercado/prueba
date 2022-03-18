298c35f2862c232b8f86f518947042bd02a27818
Rodomercado <65274543+Rodomercado@users.noreply.github.com>
2022-03-18 10:45:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,23 @@ navbar.forEach(element => {
         navbar.forEach(nav=>nav.classList.remove("active"))
 
         this.classList.add("active");
-    })
-    
+    })   
 });
+
+navbar.forEach(element => {
+    element.addEventListener("scroll", ()=> {
+        let current = '';
+        navbar.forEach(navbar => {
+            const sectionTop = navbar.offsetTop;
+            const sectionHeight = navbar.clientHeight;
+            if(pageYOffset >= sectionTop){
+                current = navbar.getAtttribute('class');
+            }
+        })
+        navbar.forEach(a => {
+            navbar.forEach(nav=>nav.classList.remove("active"))
+
+            this.classList.add("active");
+        })
+    })
+})
